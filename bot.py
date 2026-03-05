2b79d33cb0eee15b0874d84bc4ecfa4e3f8667a1
rt675158-hue <rt675158@gmail.com>
2026-03-05 15:04:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 import requests
 import pandas as pd
+import time
 
 BOT_TOKEN = "8780560298:AAHGfAMAbFtfJ-5cAWMfcfy86UiXZHPoREQ"
 CHAT_ID = "7969223643"
 
-SYMBOL = "BTCUSDT"
+SYMBOLS = ["BTCUSDT","ETHUSDT","XRPUSDT","SOLUSDT"]
 
 def send(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {"chat_id": CHAT_ID, "text": msg}
     requests.post(url,data=data)
 
-def get_klines():
+def get_klines(symbol):
 
-    url=f"https://api.binance.com/api/v3/klines?symbol={SYMBOL}&interval=15m&limit=100"
+    url=f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval=15m&limit=100"
 
     data=requests.get(url).json()
 
@@ -43,57 +44,56 @@ def rsi(series,period=14):
     return 100-(100/(1+rs))
 
 
-s=get_klines()
+while True:
 
-# SAFE CHECK
-if len(s) < 60:
-    send("Market data not available")
-    exit()
+    for symbol in SYMBOLS:
 
-ema20=ema(s,20).iloc[-1]
-ema50=ema(s,50).iloc[-1]
+        s=get_klines(symbol)
 
-rsi_val=rsi(s).iloc[-1]
+        if len(s)==0:
+            continue
 
-price=s.iloc[-1]
+        ema20=ema(s,20).iloc[-1]
+        ema50=ema(s,50).iloc[-1]
 
+        rsi_val=rsi(s).iloc[-1]
 
-if ema20>ema50 and rsi_val>55:
+        price=s.iloc[-1]
 
-    entry=price
-    sl=round(price*0.98,2)
-    tp=round(price*1.04,2)
+        if ema20>ema50 and rsi_val>55:
 
-    msg=f"""
+            entry=price
+            sl=round(price*0.98,2)
+            tp=round(price*1.04,2)
+
+            msg=f"""
 BUY SIGNAL
 
-Coin: {SYMBOL}
+Coin: {symbol}
 
 Entry: {entry}
 StopLoss: {sl}
 TakeProfit: {tp}
 """
 
-    send(msg)
+            send(msg)
 
-elif ema20<ema50 and rsi_val<45:
+        elif ema20<ema50 and rsi_val<45:
 
-    entry=price
-    sl=round(price*1.02,2)
-    tp=round(price*0.96,2)
+            entry=price
+            sl=round(price*1.02,2)
+            tp=round(price*0.96,2)
 
-    msg=f"""
+            msg=f"""
 SELL SIGNAL
 
-Coin: {SYMBOL}
+Coin: {symbol}
 
 Entry: {entry}
 StopLoss: {sl}
 TakeProfit: {tp}
 """
 
-    send(msg)
+            send(msg)
 
-else:
-
-    send("No trading signal now")
+    time.sleep(1500)
